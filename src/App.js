6bc3a1d5805f900b6796adde5890b72d9e2e7edc
Sauloleocadio <saulomanshu@gmail.com>
2019-10-17 17:53:40 -0300

    Initialized project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import logo from './assets/logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <img src={logo} alt="AirCNC"/>
+    
+
+       <div className="content">
+      <p>
+
+      </p>
+
+      </div>
     </div>
   );
 }
